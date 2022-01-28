441fc181e94855a4bce959c3cba460e38cfb9df4
Renato Sousa <94645489+SousaExm@users.noreply.github.com>
2022-01-27 21:55:20 -0300

    introduzido db com SQlite, fim da aula 3

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,21 +1,26 @@
 const Profile = require('../model/Profile')
 
+
+
 module.exports = {
-    index(req,res){
-        return res.render("profile", {profile: Profile.get()} )
+    async index(req,res){
+        const profile = await Profile.get()
+        return res.render("profile", {profile: profile} )
     },
 
-    update(req,res) {
+    async update(req,res) {
         const data = req.body
         const weeksPerYear = 52
         const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
         const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
         const monthlyTotalHours = weekTotalHours * weeksPerMonth
-        const valuehour = data["monthly-budget"] / monthlyTotalHours 
-        Profile.update({
-            ...Profile.get(),
+        const valueHour = data["monthly-budget"] / monthlyTotalHours 
+        const profile = await Profile.get()
+        
+        await Profile.update({
+            ... profile,
             ...data,
-            "value-hour": valuehour,
+            "value-hour": valueHour,
         })
         
         return res.redirect('/profile')
